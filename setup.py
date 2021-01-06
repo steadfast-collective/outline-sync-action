f5cfdae90c86c07ba0c689a1890f14aa987a097c
Ben Howes <ben@ben-howes.co.uk>
2021-01-06 12:00:32 +0000

    1.0.1 or greater is required

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(
     description="Mermaid in GitHub markdown",
     package_dir={"": "."},
     packages=["gfmd"],
-    install_requires=["marko<2", "watchdog<2"],
+    install_requires=["marko>=1.0.1,<2", "watchdog<2"],
     entry_points={"console_scripts": ["gfmd=gfmd:run"]},
 )
