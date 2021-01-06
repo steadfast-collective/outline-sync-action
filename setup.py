6e9308409ff871c450ce95dc5980eb332d0ca6c9
Ben Howes <ben@ben-howes.co.uk>
2021-01-06 11:51:38 +0000

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="GFMDiagrams",
-    version="0.1.0",
+    version="0.2.0",
     url="https://github.com/cookpad/gfm-diagram",
     author="Ben Howes",
     description="Mermaid in GitHub markdown",
