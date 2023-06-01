149efb0bc85737949a87d535b883ac56b784e168
Duncan McClean <19637309+duncanmcclean@users.noreply.github.com>
2023-06-01 17:52:36 +0100

    Change warning message

--- gfmd/__main__.py
+++ gfmd/__main__.py
@@ -28,7 +28,7 @@ outline_client = OutlineApi(
 collection_id = os.environ["OUTLINE_COLLECTION_ID"]
 
 WARNING_HEADER = f""":::info
-This page is automatically synced from [github](https://github.com/{os.getenv('GITHUB_REPOSITORY')})
+This document is automatically synced from GitHub. Please make any updates to this document in [GitHub](https://github.com/{os.getenv('GITHUB_REPOSITORY')}).
 :::
 """
 
